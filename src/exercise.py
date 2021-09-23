45d861046031cb0d9f7b8e4fa460e934309483a2
Jake Lee <jake.lee@fdmgroup.com>
2021-09-23 22:42:08 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #write your code below this line
+    numbers = []
+    while True:
+        num = int(input())
+        if (num == -1):
+            break
+        else:
+            numbers.append(num)
+    start = int(input("From where?"))
+    end  = int(input("To where?"))
+    i = start
+    while (i <= end):
+        print(numbers[i])
+        i += 1
 
 if __name__ == '__main__':
     main()
